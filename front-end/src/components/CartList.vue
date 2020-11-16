8aae5ae7f5d549d7f5cc33e25b49430b4c7bf9a6
Wasiq Malik <wasiq.malik13@gmail.com>
2020-11-16 22:57:28 +0500

    front-end complete

---
+++ front-end/src/components/CartList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div v-if="products.length > 0">
+    <CartListItem
+        v-for="product in products"
+        :key="product.id"
+        :product="product" />
+    </div>
+    <p v-else> You haven't added any product to the cart</p>
+</template>
+
+<script>
+import CartListItem from './CartListItem.vue'
+export default {
+    name: 'CartList',
+    props: ['products'],
+    components:{
+        CartListItem,
+    }
+}
+</script>
