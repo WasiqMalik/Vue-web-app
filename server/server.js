519bfef35e416a85ddf12ee37acaa08226d5f058
Wasiq Malik <wasiq.malik13@gmail.com>
2020-11-16 12:14:49 +0500

    front end initialised

--- server/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-
-app.get('/', (req, res) => res.send('Hello world'))
-app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`))
-app.get(sd)
